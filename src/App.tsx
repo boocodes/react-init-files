0e60cacba56f4c743ba26166528328cf8feb90ff
Nick Felker <felker.nick@bk.ru>
2022-11-08 11:02:31 +0600

    added all react init files

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+
+
+
+interface Props{
+
+}
+
+function App(props:Props) {
+  return(
+    <>
+       <h1>hi!</h1>
+    </>
+  )
+            
+}
+
+ 
+
+
+
+export default App;
+
